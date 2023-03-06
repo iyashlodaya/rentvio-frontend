5056fc71d70a43cd3642f5c84658e6fbf0b7935a
Yash Lodaya <shahy3050@gmail.com>
2023-03-06 23:34:22 +0530

    Redesigned categories card UI

--- projfrontend/src/core/Categories.js
+++ projfrontend/src/core/Categories.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import { FaBed, FaChair, FaCouch, FaLaptop, FaMobileAlt, FaTv } from 'react-icons/fa';
 
-const CategoryCard = ({children,categoryName="Chairs"}) => {
+const CategoryCard = ({category}) => {
     return (
-      <div className="col card mt-4 mb-4 mr-2 p-0" style={{ height: "8rem" }}>
-        <div className="card-image-top text-dark">
-          <span 
-            style={{
-              position: "absolute",
-              top: "25%",
-              left: "40%",
-              fontSize: "2rem",
-            }}
-          >
-              {children}
-          </span>
-        </div>
-        <div className="card-body">
-        <p style={{textAlign:"center", marginTop:"64px", fontSize:"14px"}}>{categoryName}</p>
-        </div>
+      <div className='text-center' style={{marginRight: '29px', width: "12rem" }}>
+          <img className='d-block w-100' style={{borderRadius: "16px", overflow: 'hidden', opacity: 1, height: "8rem", objectFit: "cover"}} src={category.imageLink}></img>
+          <p className='mt-2'>{category.name}</p>
       </div>
     );
 }
  
 class Categories extends React.Component {
     render() { 
-        return <div className="categories row">
-            <>
-            <CategoryCard categoryName={"Chairs"}>{<FaChair/>}</CategoryCard>
-            <CategoryCard categoryName={"Sofa Set"}>{<FaCouch/>}</CategoryCard>
-            <CategoryCard categoryName={"Beds"}>{<FaBed/>}</CategoryCard>
-            <CategoryCard categoryName={"Electronics"}>{<FaMobileAlt/>}</CategoryCard>
-            <CategoryCard categoryName={"Laptops"}>{<FaLaptop/>}</CategoryCard>
-            <CategoryCard categoryName={"Television"}>{<FaTv/>}</CategoryCard>
-            </>
+        return <div className='d-flex justify-content-center pt-4'>
+            <CategoryCard category={{name: 'Bedroom', imageLink: 'https://images.unsplash.com/photo-1585821569331-f071db2abd8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'}} />
+            <CategoryCard category={{name: 'Living Room', imageLink: 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80'}} />
+            <CategoryCard category={{name: 'Work From Home', imageLink: 'https://images.unsplash.com/photo-1492138786289-d35ea832da43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'}} />
+            <CategoryCard category={{name: 'Kitchen', imageLink: 'https://images.unsplash.com/photo-1628843226223-989e20810393?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'}} />
         </div>;
     }
 }
