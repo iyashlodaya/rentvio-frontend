6f280292b70a23a442c2b32c74b04058581ace85
Yash Lodaya <shahy3050@gmail.com>
2023-03-08 01:45:46 +0530

    added product card and product section on homepage

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../styles.css";
 import Base from "./Base";
-import photo from "./couch.jpg"
-import couchPhoto from "./unsplash.jpg"
 import Categories from "./Categories";
 import { Carousel } from "react-bootstrap";
+import ProductCard from "./ProductCard";
 
 const carousalImages = {
   imageOne: 'https://images.unsplash.com/photo-1594614271360-0ed9a570ae15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -44,49 +43,108 @@ const renderCarousalForHeroSection = () => {
 }
 
 const Home = () => {
-  // const products = [1, 2, 3,4,5,6,7,8];
+  const scroll = useRef(null);
+  const [scrollValue, setScrollValue] = useState(0);
+  const [scrollEnd, setScrollEnd] = useState(false);
+
+  const slide = (shift) => {
+    scroll.current.scrollLeft += shift;
+    setScrollValue(scrollValue + shift);
+
+    if (
+      Math.floor(scroll.current.scrollWidth - scroll.current.scrollLeft) <=
+      scroll.current.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+
+  const handleScroll = () => {
+    setScrollValue(scroll.current.scrollLeft);
+    if (
+      Math.floor(scroll.current.scrollWidth - scroll.current.scrollLeft) <=
+      scroll.current.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+  const products = [
+    {
+      productName: "Orange Chair",
+      productPrice: '₹ 499/mo.',
+      productImageLink:
+        "https://images.unsplash.com/photo-1506332088442-9e0024864f5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      productName: "Bed and Mattress",
+      productImageLink:
+        "https://images.pexels.com/photos/6489083/pexels-photo-6489083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      productName: "Yellow Sofa Set",
+      productImageLink:
+        "https://images.unsplash.com/photo-1558211583-d26f610c1eb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1106&q=80",
+    },
+    {
+      productName: "Table & Chair Set",
+      productImageLink:
+        "https://images.pexels.com/photos/271696/pexels-photo-271696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      productName: "Vintage Refrigerator",
+      productImageLink:
+        "https://images.pexels.com/photos/2962002/pexels-photo-2962002.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      productName: "Laptop",
+      productImageLink:
+        "https://images.pexels.com/photos/5793953/pexels-photo-5793953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      productName: "Dining Table Set",
+      productImageLink:
+        "https://images.pexels.com/photos/932095/pexels-photo-932095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      productName: "Coffee Maker",
+      productImageLink:
+        "https://images.unsplash.com/photo-1565452344518-47faca79dc69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    },
+  ];
   return (
-    <Base navbar={true} footer={false} >
+    <Base navbar={true} footer={true} >
       <div className="container">
-        <div className="row">
-          <div className="col p-0">
-            {renderCarousalForHeroSection()}
+          <div className="row">
+            <div className="col p-0">
+              {renderCarousalForHeroSection()}
+            </div>
           </div>
-        </div>
 
           <div className="text-center pt-4">
             <h2 style={{color:'#101721'}}>Browse by category</h2>
             <h4 style={{color:'#8D8E91', fontWeight: '300'}}>Furniture rentals for your home and office.</h4>
           </div>
-
           <Categories/>
 
-        {/* <div className="randomProducts row">
-          {products.map((product, index) => {
-            return (
-              <div key={index} className="col m-0 p-0 text-center d-flex justify-content-center">
-                <div className="card m-2" style={{ width: "16rem" }}>
-                  <img
-                    className="card-img-top"
-                    src={couchPhoto}
-                    alt="Card imagecap"
-                  />
-                  <div className="card-body text-dark">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">₹550</p>
-                    <a
-                      href="https://www.google.com"
-                      className="btn btn-outline-primary"
-                    >
-                      View More
-                    </a>
-                  </div>
-                </div>
+          <div className="d-flex justify-content-between">
+              <h4 style={{color:'#101721'}}>Modern furniture right at your doorstep!</h4>
+              <div>
+                <button id="nav-btn" className="btn" onClick={()=>{slide(-50)}}><i className="fa fa-angle-left"></i></button>
+                <button id="nav-btn" className="btn" onClick={()=>slide(+50)}><i className="fa fa-angle-right"></i></button>
               </div>
-            );
-          })}
-        </div> */}
-      </div>
+          </div>
+          <div ref={scroll} onScroll={handleScroll} className="product-section pt-4">
+            { products.map((product, index) => {
+              return (
+                <ProductCard productInfo={product} key={index}/>
+              );
+            })}
+          </div>
+        </div>
     </Base>
   );
 };
---
+++ projfrontend/src/core/ProductCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function ProductCard({productInfo}) {
+  const history = useHistory();
+  return (
+    <div
+      className="text-center me-5"
+      id="product-card"
+      style={{ width: "16rem" }}
+    >
+      <img
+        id="product-image"
+        src={productInfo.productImageLink}
+        style={{
+          width: "16rem",
+          height: "18rem",
+          objectFit: "cover",
+          borderRadius: "2%",
+        }}
+      />
+      <div className="product-body d-flex justify-content-between pt-3">
+        <div className="d-flex flex-column text-start">
+        <p id="product-name" style={{fontSize: "14px", margin: 0, fontWeight:"500"}}>
+          {productInfo.productName}
+        </p>
+        <p id="product-price" style={{fontSize: "12px", fontWeight:"500"}}>
+          ₹ 499/mo
+        </p>
+        </div>
+        <button id="add-to-cart-btn" className="btn text-center" onClick={()=>{history.push('/login')}}>
+          <span style={{fontSize: "14px"}}>Rent <i className="ps-1 fa fa-shopping-bag fa-sm"></i></span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
